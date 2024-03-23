f65c834bac851135fd8b8c1b8c60a36d68ffc7d7
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-03-23 11:38:22 +0530

    remove import line

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { ArrowMonth } from "./Icon";
 import Image from "next/image";
 
 const Work = () => {
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client"
 import { ArrowMonth } from "@/Components/Icon";
 import Navbar from "@/Components/Navbar";
-import Topic from "@/Components/Topic";
 import Work from "@/Components/Work";
 import Image from "next/image";
 import Link from "next/link";
